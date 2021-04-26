d2862bc5842b591549ab200f089bca3420e56aba
ASmileyDude <81973583+ASmileyDude@users.noreply.github.com>
2021-04-26 16:44:34 +0100

    Update index.js

--- index.js
+++ index.js
@@ -40,47 +40,4 @@ client.on('message', async (message) => {
 }
 });
 
-client.login(token)
-const discord = require('discord.js')
-const client = new discord.Client
-const prefix = "."
-
-client.on('ready', () =>{
-    console.log(`${client.user.username} is now online!`)
-})
-
-client.on('message', async (message) => {
-    if (
-      message.content.toLowerCase().startsWith(prefix + 'clear') ||
-      message.content.toLowerCase().startsWith(prefix + 'c ')
-    ) {
-      if (!message.member.hasPermission('MANAGE_MESSAGES'))
-        return message.channel.send("You cant use this command since you're missing `manage_messages` perm");
-      if (!isNaN(message.content.split(' ')[1])) {
-        let amount = 0;
-        if (message.content.split(' ')[1] === '1' || message.content.split(' ')[1] === '0') {
-          amount = 1;
-        } else {
-          amount = message.content.split(' ')[1];
-          if (amount > 100) {
-            amount = 100;
-          }
-        }
-  
-        await message.delete().catch(e => { amount++; });
-  
-        await message.channel.bulkDelete(amount, true).then((_message) => {
-          const Clearembed = new discord.MessageEmbed()
-          .setTitle(`**CLEARED**`)
-          .setDescription(`${message.author.username} cleared ${_message.size} messages!`)
-          message.channel.send(Clearembed).then((sent) => {
-            setTimeout(function () {
-              sent.delete();
-            }, 2000);
-          });
-        });
-    }
-}
-});
-
 client.login(process.env.TOKEN)
